0060c38c5f84bb1b3a624950c3061a9af0b4b45b
Andrew Miracle <me@andrewmiracle.com>
2025-06-08 23:14:54 +0000

    fix(web): quick updates to root styling and font directory

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,23 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Ubuntu_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from '@/utils/provider';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
 });
+const ubuntu = Ubuntu_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-ubuntu",
+});
+
 
 export const metadata: Metadata = {
   title: "Leadmark - Email-First Collaboration | Postmark Challenge",
@@ -24,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${ubuntu.variable} ${geistMono.variable}`}>
         <Providers>{children}</Providers>
       </body>
     </html>
